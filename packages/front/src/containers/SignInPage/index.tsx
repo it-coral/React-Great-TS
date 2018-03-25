51f3b26de2bc499e21fe072ec13ba1c6679b9981
it-coral <coral1212@yahoo.com>
2018-03-25 12:53:57 +0300

    Added final-form

--- packages/front/src/containers/SignInPage/index.tsx
+++ packages/front/src/containers/SignInPage/index.tsx
@@ -1,16 +1,48 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
 
-interface ILoginPageProps {
+interface LoginPageProps {
     test: string;
 }
 
-class SignInPage extends React.Component<ILoginPageProps> {
+class SignInPage extends React.Component<LoginPageProps> {
     public render(): JSX.Element {
         return (
             <div>
                 <h1>SignIn page</h1>
-                <Link to={'/signup'}>Go to sign-up page</Link>
+                <Link to={'/sign-up'}>Go to sign-up page</Link>
+                <Grid container={true}>
+                    <Grid item={true} xs={12}>
+                        <Grid
+                            container={true}
+                            alignItems="center"
+                            direction="row"
+                            justify="center"
+                        >
+                            <Paper elevation={4}>
+                                <Grid container={true}>
+                                    <Grid item={true} xs={12}>
+                                        <TextField
+                                            id="uncontrolled"
+                                            label="Login"
+                                            margin="normal"
+                                        />
+                                    </Grid>
+                                    <Grid item={true} xs={12}>
+                                        <TextField
+                                            id="uncontrolled"
+                                            label="Password"
+                                            margin="normal"
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
