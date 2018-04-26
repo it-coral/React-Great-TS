a51e51f6419b9a34cd36a7b0afd90916ccbc6d83
it-coral <coral1212@yahoo.com>
2018-04-26 20:15:20 +0300

    Added select field. Aded form to toolbar

--- packages/front/src/components/common/Grid/SearchToolbar.tsx
+++ packages/front/src/components/common/Grid/SearchToolbar.tsx
@@ -1,87 +1,138 @@
 import * as React from 'react';
 import { Theme, withStyles, WithStyles } from 'material-ui/styles';
 import {
-  TableCell,
+    TableCell,
 } from 'material-ui/Table';
 import TextField from 'material-ui/TextField';
 import IconButton from 'material-ui/IconButton';
 import { InputAdornment } from 'material-ui/Input';
 import Search from 'material-ui-icons/Search';
 import Cancel from 'material-ui-icons/Cancel';
+import { FormControl } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+import { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import { Field, FieldRenderProps, Form } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
+import { FieldArray } from 'react-final-form-arrays';
+import TextFieldControl from '../form-elements/TextFieldControl';
 
-type StyledComponent = WithStyles<
-  'searchField' |
-  'searchCell' |
-  'searchBtn'
-  >;
+type StyledComponent = WithStyles<'searchField' |
+    'searchCell' |
+    'searchBtn'>;
 
 export interface SearchToolbarProps {
-  value: string;
-  placeholder: string;
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  onResetSearch: () => void;
-  onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    value: string;
+    placeholder: string;
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    onResetSearch: () => void;
+    onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 class SearchToolbar extends React.Component<SearchToolbarProps & StyledComponent> {
-  render() {
-    const {
-      classes,
-      onSearchChange,
-      onResetSearch,
-      onSubmit,
-      placeholder,
-      value
-    } = this.props;
-    return (
-      <TableCell
-        className={classes.searchCell}
-      >
-        <form onSubmit={onSubmit}>
-          <TextField
-            value={value}
-            onChange={onSearchChange}
-            className={classes.searchField}
-            placeholder={placeholder}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">
-                {value   &&
-                  <IconButton
-                    className={classes.searchBtn}
-                    aria-label="clear"
-                    onClick={onResetSearch}
-                  >
-                    <Cancel />
-                  </IconButton>}
-                <IconButton
-                  className={classes.searchBtn}
-                  type="submit"
-                  aria-label="search"
-                >
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            }}
-          />
-        </form>
-      </TableCell>
-    );
-  }
+    render() {
+        const {
+            classes,
+            onResetSearch,
+            onSubmit,
+            placeholder,
+        } = this.props;
+        return (
+            <TableCell
+                className={classes.searchCell}
+            >
+                <Form
+                    onSubmit={onSubmit}
+                    initialValues={{filter: [{}]}}
+                    mutators={{
+                        ...arrayMutators
+                    }}
+                    render={({handleSubmit, submitError}) => (
+                        <form
+                            onSubmit={handleSubmit}
+                        >
+                            <FieldArray name="filter">
+                                {({ fields }) =>
+                                    fields.map((name, index) => (
+                                        <React.Fragment key={index}>
+                                            <Field
+                                                component={TextFieldControl}
+                                                type="number"
+                                                name={`${name}.field`}
+                                                label="Filed"
+                                            />
+                                        </React.Fragment>
+                                    ))}
+                            </FieldArray>
+
+                            <FormControl>
+                                <InputLabel>Age</InputLabel>
+                                <Select
+                                    value={20}
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'age-simple',
+                                    }}
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Field
+                                type="text"
+                                name={`searchField`}
+                                label="Filed"
+                                render={(props: FieldRenderProps) => (
+                                    <TextField
+                                        value={props.input.value}
+                                        onChange={props.input.onChange}
+                                        className={classes.searchField}
+                                        placeholder={placeholder}
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">
+                                                {props.input.value &&
+                                                    <IconButton
+                                                        className={classes.searchBtn}
+                                                        aria-label="clear"
+                                                        onClick={onResetSearch}
+                                                    >
+                                                        <Cancel/>
+                                                    </IconButton>
+                                                }
+                                                    <IconButton
+                                                        className={classes.searchBtn}
+                                                        type="submit"
+                                                        aria-label="search"
+                                                    >
+                                                    <Search/>
+                                                    </IconButton>
+                                            </InputAdornment>
+                                        }}
+                                    />
+                                )}
+                            />
+                        </form>
+                    )}
+                />
+            </TableCell>
+        );
+    }
 }
 
 const styles = (theme: Theme) => ({
-  searchField: {
-    width: 245,
-    marginRight: theme.spacing.unit
-  },
-  searchCell: {
-    paddingRight: 0,
-    minWidth: 373
-  },
-  searchBtn: {
-    width: 30,
-    height: 30
-  }
+    searchField: {
+        width: 245,
+        marginRight: theme.spacing.unit
+    },
+    searchCell: {
+        paddingRight: 0,
+        minWidth: 373
+    },
+    searchBtn: {
+        width: 30,
+        height: 30
+    }
 } as React.CSSProperties);
 
 const decorate = withStyles(styles);
---
+++ packages/front/src/components/common/form-elements/SelectFieldControl.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import { FieldRenderProps } from 'react-final-form';
+import withStyles from 'material-ui/styles/withStyles';
+import { WithStyles } from 'material-ui/styles';
+import Select from 'material-ui/Select';
+import { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+
+interface ISelectOption {
+    value: string | number;
+    label: string;
+}
+
+interface ISelectFieldControl {
+    label?: string;
+    emptyOption?: boolean;
+    options: Array<ISelectOption>;
+}
+
+class SelectFieldControl extends React.Component<FieldRenderProps & ISelectFieldControl & WithStyles<'root'>> {
+    public render(): JSX.Element {
+        const {
+            classes,
+            input: {
+                name,
+                onChange,
+                value,
+                ...restInput
+            },
+            meta,
+            label,
+            emptyOption,
+            options,
+            ...rest
+        } = this.props;
+
+        return (
+            <FormControl
+                {...rest}
+                className={classes.root}
+            >
+                <InputLabel>{label}</InputLabel>
+                <Select
+                    inputProps={restInput}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+
+                >
+                    {emptyOption &&
+                        <MenuItem value="">
+                            <em>{label}</em>
+                        </MenuItem>
+                    }
+                    {options.map((o, index) => {
+                        <MenuItem key={index} value={o.value}>{o.label}</MenuItem>
+                    })}
+                </Select>
+                {((meta.error || meta.submitError) && meta.touched) &&
+                    <FormHelperText>{(meta.error || meta.submitError)}</FormHelperText>
+                }
+            </FormControl>
+        );
+    }
+}
+
+const decorate = withStyles(() => ({
+    root: {
+        width: '100%',
+        marginTop: 0,
+        marginBottom: 0
+    }
+}));
+
+export default decorate<FieldRenderProps>(SelectFieldControl);
